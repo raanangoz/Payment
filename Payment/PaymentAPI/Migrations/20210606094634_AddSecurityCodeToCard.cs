e0380f2fc3c2ca61e9c9007376c69618a712f9ab
Raanan Gozlan <raanangoz@gmail.com>
2021-06-11 17:09:47 +0300

    added migrations

---
+++ Payment/PaymentAPI/Migrations/20210606094634_AddSecurityCodeToCard.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentAPI.Migrations
+{
+    public partial class AddSecurityCodeToCard : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SecurityCode",
+                table: "PaymentDetails",
+                type: "nvarchar(3)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SecurityCode",
+                table: "PaymentDetails");
+        }
+    }
+}
